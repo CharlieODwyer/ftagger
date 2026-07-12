b3fca52e3d64f21f7a501113b1ede82532a3b349
Charlie <charlieodwyer2289@gmil.com>
2026-07-12 21:06:30 +0100

    Added the check_for_first_run() utility

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,70 @@
 import ansi_codes as ac
+import json
+
+from globals import *
+
+
+def check_for_first_run():
+    if not Path.exists(f"{Path.home()}/.fmdata"):
+        with open(globals.FMALIASLOOKUP, "w") as file:
+            json.dump({}, file, indent=4)
+
+
+        with open(globals.FMALIASES, "w") as file:
+            json.dump({}, file, indent=4)
+
+
+        with open(globals.FMDEFAULT, "w") as file:
+            default_data: dict = {
+                "--help": "help",
+                "at": "add_tag",
+                "rt": "remove_tag",
+                "gi": "get_items_by_tag",
+                "gt": "get_tags",
+                "li": "list_items_and_tags",
+                "rc": "recursive_add_tag",
+                "rr": "recursive_remove_tag",
+                "ca": "create_alias",
+                "ra": "remove_alias",
+                "la": "list_aliases",
+                "rd": "reset_data",
+                "un": "undo"
+            }
+
+            json.dump(default_data, file, indent=4)
+
+
+        with open(globals.FMUNDOLOOKUP) as file:
+            default_data: dict = {
+                "at": "rt",
+                "rt": "at",
+                "rc": "rr",
+                "rr": "rc",
+                "ca": "ra",
+                "ra": "ca",
+                "tag": "rt"
+            }
+
+
+        with open(globals.FMPREVIOUSCOMMAND, "w") as file:
+            default_data: dict = {
+                "0": [
+                    "Default",
+                ],
+                "1": [
+                    "Default",
+                ],
+                "2": [
+                    "Default",
+                ],
+                "3": [
+                    "Default",
+                ],
+                "4": [
+                    "Default",
+                ]
+            }
+
 
 def error(text: str) -> None:
     print(f"{ac.red}{text}{ac.clear}")
