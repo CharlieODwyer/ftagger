038e1c31e9b58d2fad93c74c7170b2d1af1da0aa
Charlie <charlieodwyer2289@gmil.com>
2026-07-12 21:59:17 +0100

    The paths fixed in the last commit weren't actually invalid :/

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 FMDATA = Path.home() / ".fmdata"
-FMDEFAULT = f"{Path.home()}.fmdata/.fmdefault"
-FMALIASES = f"{Path.home()}.fmdata/.fmaliases"
-FMALIASLOOKUP = f"{Path.home()}.fmdata/.fmaliaslookup"
-FMUNDOLOOKUP = f"{Path.home()}.fmdata/.fmundolookup"
-FMPREVIOUSCOMMAND = f"{Path.home()}.fmdata/.fmpreviouscommand"
+FMDEFAULT = f"{Path.home()}/.fmdata/.fmdefault"
+FMALIASES = f"{Path.home()}/.fmdata/.fmaliases"
+FMALIASLOOKUP = f"{Path.home()}/.fmdata/.fmaliaslookup"
+FMUNDOLOOKUP = f"{Path.home()}/.fmdata/.fmundolookup"
+FMPREVIOUSCOMMAND = f"{Path.home()}/.fmdata/.fmpreviouscommand"
